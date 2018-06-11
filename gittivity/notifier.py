f838a0c4582e51ff9ea3300bf67da503e6bd0792
s1s1ty <shaonty.dutta@gmail.com>
2018-06-11 16:20:06 +0600

    generalize

--- gittivity/notifier.py
+++ gittivity/notifier.py
@@ -1,13 +1,11 @@
-import requests
+import sys
+import os
 import argparse
 from time import sleep
 
+import requests
 from pync import notify
 from pyjsonq import JsonQ
-
-parser = argparse.ArgumentParser(description="Get user's github username.")
-parser.add_argument("github_handle", type=str, help="Type github username")
-args = parser.parse_args()
 
 
 def _match(property):
@@ -43,6 +41,51 @@ def _match(property):
         return ""
 
     return event_mapper.get(property)
+
+
+def help_text():
+    """Provide instruction about useage"""
+    print(
+        "Please follow the instruction\n"
+        "* Use argument `-c config` to config github handle\n"
+        "* Use argument `-s start` to start application\n"
+        "* Use argument `-k kill` to kill application\n"
+        "* Use argument `-i hint` for instruction\n"
+    )
+
+
+def start():
+    """Start gittivity"""
+    main()
+
+
+def stop():
+    """Stop gittivity"""
+    os.system("pkill -9 -f notifier.py")
+
+
+def configure():
+    with open("config.txt") as f:
+        line = f.readline().split(":")
+    return None if not line else line[1]
+
+
+github_handle = configure()
+
+
+def parse_arguments():
+    """Return parsed argument"""
+    parser = argparse.ArgumentParser(
+        description='Github desktop notifier',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter
+    )
+
+    parser.add_argument("-s", "--start", type=str, help="start gittivity")
+    parser.add_argument("-k", "--kill", type=str, help="stop gittivity")
+    parser.add_argument("-c", "--config", type=str, help="configure git profile")
+    parser.add_argument("-i", "--hint", type=str, help="provide instruction")
+
+    return parser.parse_args()
 
 
 def event_notifier(data, old_notify_time):
@@ -82,14 +125,14 @@ def event_notifier(data, old_notify_time):
 
 
 def main():
+    print("checking...")
     old_notify_time = ""
 
     while True:
-        print("checking...")
         try:
             handle_link = "{}{}{}".format(
                 "https://api.github.com/users/",
-                args.github_handle,
+                github_handle,
                 "/received_events"
             )
 
@@ -105,4 +148,25 @@ def main():
 
 
 if __name__ == '__main__':
-    main()
+    try:
+        arg = parse_arguments()
+        if arg.kill == "kill":
+            stop()
+
+        if arg.start == "start":
+            if github_handle:
+                start()
+            else:
+                print("Please configure first. See help `-i hint`")
+
+        if arg.config == "config":
+            if not github_handle:
+                github_handle = input("Your github handle: ")
+                with open("config.txt", "w") as text_file:
+                    text_file.write("github_handle:{}".format(github_handle))
+            start()
+
+        if arg.hint == "hint":
+            help_text()
+    except:
+        help_text()
